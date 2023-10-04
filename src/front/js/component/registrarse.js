a1c9ecec410e9c8f10a3a2738681b359c25e0021
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-10-04 00:47:13 +0000

    cambios en nextEvent

--- src/front/js/component/registrarse.js
+++ src/front/js/component/registrarse.js
@@ -135,7 +135,7 @@ const Registrarse = () => {
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <PayPal />
+                            <PayPal total={total}/>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
