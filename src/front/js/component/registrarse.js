51030e9ac4f0113461d9008160a247bd0f2f031b
jarkor5 <67030533+jarkor5@users.noreply.github.com>
2023-10-12 00:36:31 +0000

    agregado modal para editar datos, arreglados conflictos

--- src/front/js/component/registrarse.js
+++ src/front/js/component/registrarse.js
@@ -217,22 +217,81 @@ const Registrarse = (props) => {
       
               
       <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-                <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div className="modal-body">
-                        <PayPal />
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
-                    </div>
-                </div>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
+            <div className="modal-body">
+              <div className="modal-body">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <h3>Cantidad de equipos a registrar</h3>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3">
+                      <b><h5>Evento</h5></b>
+                    </div>
+                    <div className="col-md-3">
+                      <b><h5>Costo</h5></b>
+                    </div>
+                    <div className="col-md-3">
+                      <b><h5>Cant.</h5></b>
+                    </div>
+                    <div className="col-md-3">
+                      <b><h5>Total</h5></b>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-md-3">
+                      <p>Jam on it</p>
+                    </div>
+                    <div className="col-md-3">
+                      <p>${datosEvento.costo}</p>
+                    </div>
+                    <div className="col-md-3">
+                      <button className="button" id="disminuir" onClick={disminuir}>-</button>
+                      <span><button id="cantidad" value="0">{contador}</button></span>
+                      <button className="button" id="aumentar" onClick={aumentar}>+</button>
+                    </div>
+                    <div className="col-md-3">
+                      <p>${total}</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-3"></div>
+                    <div className="col-md-3">
+                      <b><h4>Total</h4></b>
+                    </div>
+                    <div className="col-md-3">
+                      <b><h4>${total}</h4></b>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-3"></div>
+                    <div className="col-md-3"></div>
+                    <div className="col-md-3">
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <PayPal index={params.index} costo={total} />
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Save changes</button>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
   );
 };
