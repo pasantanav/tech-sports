6b6789d5d2d89bf4394e4af79518cfa48fdacb7e
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-10-13 05:49:54 +0000

    algunos cambios de la galeria

--- src/front/js/component/galeria.js
+++ src/front/js/component/galeria.js
@@ -1,91 +1,146 @@
-import React from "react"
-import "../../styles/home.css";
-import basket from "../../img/basket7.jpeg";
+import React, { useState } from 'react';
+import Lightbox from 'yet-another-react-lightbox';
+import 'yet-another-react-lightbox/styles.css';
+ import "../../styles/home.css";
+ import basket from "../../img/basket7.jpeg";
+ import basket5 from "../../img/basket5.jpeg";
+ import basket12 from "../../img/basket12.jpg";
+ import basket13 from "../../img/basket13.jpg";
+ import basket14 from "../../img/basket14.jpeg";
+ import basket15 from "../../img/basket15.jpg";
+ import basket16 from "../../img/basket16.jpg";
+ import lebron17 from "../../img/lebron17.jpg";
+ import basket6 from "../../img/basket6.jpeg";
+ import basket22 from "../../img/basket22.jpeg";
+ import basket23 from "../../img/basket23.jpeg";
+ import basket24 from "../../img/basket24.jpeg";
+ import basket25 from "../../img/basket25.jpeg";
+ import basket26 from "../../img/basket26.jpeg";
+ import basket28 from "../../img/basket28.jpeg";
+ import basket29 from "../../img/basket29.jpeg";
+ import basket31 from "../../img/basket31.jpeg";
+ import basket32 from "../../img/basket32.jpeg";
+ import basket33 from "../../img/basket33.jpeg";
+ import basket34 from "../../img/basket34.jpeg";
+ import basket35 from "../../img/basket35.jpeg";
+ import basket36 from "../../img/basket36.jpeg";
+ import basket37 from "../../img/basket37.jpeg";
+ import mascotas from "../../img/mascotas.jpeg";
+ import mascotas2 from "../../img/mascotas2.jpeg";
+ import mascotas3 from "../../img/mascotas3.jpeg";
+ import mascotas4 from "../../img/mascotas4.jpeg";
+ import mascotas5 from "../../img/mascotas5.jpeg";
+ import mascotas6 from "../../img/mascotas6.jpeg";
+ import mascotas7 from "../../img/mascotas7.jpeg";
+ 
+ const Galeria = () => {
+  const [open, setOpen] = React.useState(false);
+   return (
+     <div className="contSuperior container">
+       <div className="ecommerce-gallery" data-mdb-zoom-effect="true" data-mdb-auto-height="true">
+         <div className="row">
+           <div style={{ textAlign: "center" }} className="col-12">
+             <h1>Galería</h1>
+             <div className="divider divider-default m-3"></div>
+           </div>
+         </div>
+         <div className="row py-3 shadow-5">
+           <div className="col-3 mb-1">
+             <div className="lightbox">
+               <img
+                 src={basket5}
+                 alt="Gallery image  1"
+                 className="ecommerce-gallery-main-img active w-100"
+               />
+             </div>
+           </div>
+           <div className="col-3 mt-1">
+             <img  id="image12"
+               src={basket12}
 
-import basket5 from "../../img/basket5.jpeg";
-import basket12 from "../../img/basket12.jpg";
-import basket13 from "../../img/basket13.jpg";
-import basket14 from "../../img/basket14.jpeg";
-import basket15 from "../../img/basket15.jpg";
-import basket16 from "../../img/basket16.jpg";
-import lebron17 from "../../img/lebron17.jpg";
-import basket6 from "../../img/basket6.jpeg";
-const Galeria = () => {
-  return (
-    <div className="contSuperior container">
-      <div className="ecommerce-gallery" data-mdb-zoom-effect="true" data-mdb-auto-height="true">
-        <div className="row">
-          <div style={{ textAlign: "center" }} className="col-12">
-            <h1>Galería</h1>
-            <div className="divider divider-default m-3"></div>
-          </div>
-        </div>
-        <div className="row py-3 shadow-5">
-          <div className="col-3 mb-1">
-            <div className="lightbox">
-              <img
-                src={basket5}
-                alt="Gallery image  1"
-                className="ecommerce-gallery-main-img active w-100"
-              />
-            </div>
-          </div>
-          <div className="col-3 mt-1">
-            <img  id="image12"
-              src={basket12}
+               alt="Gallery image 1"
+               className="active w-100"
+             />
+           </div>
+           <div className="col-3 mt-1">
+             <img  id="image13"
+               src={basket13}
 
-              alt="Gallery image 1"
-              className="active w-100"
-            />
-          </div>
-          <div className="col-3 mt-1">
-            <img  id="image13"
-              src={basket13}
+               alt="Gallery image 1"
+               className="active w-100"
+             />
+           </div>
+           <div className="col-3 mt-1">
+             <img id="image14"
+               src={basket14}
 
-              alt="Gallery image 1"
-              className="active w-100"
-            />
-          </div>
-          <div className="col-3 mt-1">
-            <img id="image14"
-              src={basket14}
+               alt="Gallery image 1"
+               className="active w-100"
+             />
+           </div>
+           <div className="col-3 mt-1">
+             <img id="image"
+               src={basket15}
+               alt="Gallery image 1"
+               className="active w-100"
+             />
+           </div>
+           <div className="col-3 mt-1">
+             <img id="image"
+               src={basket16}
+               alt="Gallery image 2"
+               className="active w-100"
+             />
+           </div>
+           <div className="col-3 mt-1">
+             <img id="image"
+               src={lebron17}
+               alt="Gallery image 3"
+               className="active w-100"
+             />
+           </div>
+           <div className="col-3 mt-1">
+             <img id="image"
+               src={basket6}
+               alt="Gallery image 4"
+               className="active w-100"
+             />
+           </div>
+         </div>
+       </div>
+       <button type="button" onClick={() => setOpen(true)}>
+        Open Lightbox
+      </button>
 
-              alt="Gallery image 1"
-              className="active w-100"
-            />
-          </div>
-          <div className="col-3 mt-1">
-            <img id="image"
-              src={basket15}
-              alt="Gallery image 1"
-              className="active w-100"
-            />
-          </div>
-          <div className="col-3 mt-1">
-            <img id="image"
-              src={basket16}
-              alt="Gallery image 2"
-              className="active w-100"
-            />
-          </div>
-          <div className="col-3 mt-1">
-            <img id="image"
-              src={lebron17}
-              alt="Gallery image 3"
-              className="active w-100"
-            />
-          </div>
-          <div className="col-3 mt-1">
-            <img id="image"
-              src={basket6}
-              alt="Gallery image 4"
-              className="active w-100"
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={[
+          { src: basket22 },
+          { src:basket23  },
+          { src: basket24 },
+          { src: basket25 },
+          { src:basket26 },
+          { src: basket28 },
+          { src: basket29 },
+          { src:basket31 },
+          { src: basket32 },
+          { src: basket33 },
+          { src:basket34  },
+          { src: basket35 },
+          { src: basket36 },
+          { src:basket37},
+          { src: mascotas },
+          { src: mascotas2 },
+          { src:mascotas3 },
+          { src: mascotas4 },
+          { src: mascotas5 },
+          { src:mascotas6 },
+          { src: mascotas7 }
+        ]}
+      />
+     </div>
+   );
+ }
 
-export default Galeria;
+ export default Galeria;
